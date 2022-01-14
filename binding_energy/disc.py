2613f788e22fa2157d1577421f2d697f52a8398a
Ignasi Puch Giner <ignasi.puch.giner@gmail.com>
2022-01-14 14:08:33 +0100

    pelesteps_retriever and column_retriever implemented in the case no flag is used to specify them.

--- binding_energy/disc.py
+++ binding_energy/disc.py
@@ -46,9 +46,9 @@ def parse_args(args):
     parser.add_argument("-T", "--temperature", type=float, dest="temperature",
                         default=298., help="Temperature of the experiment.")
     parser.add_argument("-pS", "--pele_Steps", type=int, dest="pele_steps",
-                        default=25, help="Number of Pele Steps in the simulation.")
+                        default=None, help="Number of Pele Steps in the simulation.")
     parser.add_argument("-c", "--column", type=int, dest="column",
-                        default=5, help="Column of the report where the interesting\
+                        default=None, help="Column of the report where the interesting\
                              metric is located.")
 
     parsed_args = parser.parse_args(args)
@@ -77,9 +77,14 @@ def statistics(input_folder,
         Temperature to perform the Boltzmann weights with.
     - pele_steps : int
         Number of pele_steps in the simulation.
+    - column : int 
+        Column where the interesting data is located.
     """
 
-    def reader(files, folderpath, column):
+    def reader(files, 
+               folderpath, 
+               report_name,
+               column):
         """
         Function
         ----------
@@ -91,6 +96,11 @@ def statistics(input_folder,
             The path to the directory where the output of the PELE simulation is located.
         - folderpath : str
             Path where the different epochs of the simulation are located.
+        - report_name : str
+            Name of the reports to obtain the data from
+        - column : int 
+            Column where the interesting data is located.
+    
 
         Returns
         ----------
@@ -128,13 +138,12 @@ def statistics(input_folder,
                 Steps associated to poses for all the simulation.
             """
 
-            for file in files:
+            new_directory = folderpath
 
-                new_directory = folderpath
+            for file in files:
 
                 cont = 0
 
-                # Checking whether path exists and the file begins with the report_name selected
                 if file.startswith(report_name):
 
                     file_path = os.path.join(new_directory, file)
@@ -142,18 +151,69 @@ def statistics(input_folder,
                     with open(file_path, 'r') as filein:
 
                         for line in filein:
+                        
+                            line = line.split('    ')
+
                             if cont != 0:
-                                line = line.split('   ')
+                                
                                 be.append(float(line[column-1]))
                                 step.append(int(line[1]))
+
                             cont += 1
+
             return be, step
+
+        def column_retriever(file,
+                             column):
+
+            """
+            Function
+            ----------
+            Retrieves the position of the binding energy.
+
+            Parameters
+            ----------
+            - file : list
+                The path to the one report.
+            - column : int 
+                Value give to the --column flag. 
+
+            Returns
+            ----------
+            - column : int 
+                Column where the interesting data is located. 
+            """
+
+            cont = 0
+
+            with open(file, 'r') as filein:
+                
+                for line in filein:
+                
+                    if cont == 0:
+                    
+                        line = line.split('    ')                         
+                        column = line.index('BindingEnergy') + 1
+
+                        #
+                        print(' -   No column introduced, picking BindingEnergy column:', column - 1) 
+                        #
+
+                    cont += 1
+
+            return column
 
         be = []
         step = []
         numeric_files = [s for s in files if s.isnumeric()]
 
         if len(numeric_files) != 0:
+
+            if column == None:
+                
+                column_file = os.path.join(folderpath,numeric_files[0], report_name + '1')
+                column = column_retriever(column_file,
+                                          column)
 
             for document in numeric_files:
 
@@ -169,8 +229,11 @@ def statistics(input_folder,
                         raise Exception('FilePathError: There is no file containing ' + report_name + ' in it. \
                         Please check the path to the files and the files name.')
 
-                    be, step = file_reader(
-                        files, new_directory, report_name, column)
+
+                    be, step = file_reader(files,
+                                           new_directory, 
+                                           report_name, 
+                                           column)
 
         else:
 
@@ -178,11 +241,22 @@ def statistics(input_folder,
                 raise Exception('FilePathError: There is no file containing ' + report_name + ' in it. \
                 Please check the path to the files and the files name.')
 
-            be, step = file_reader(files, folderpath, report_name, column)
+            if column == None:
+                
+                column_file = os.path.join(folderpath, report_name + '1')
+                column = column_retriever(column_file,
+                                          column)
+
+            be, step = file_reader(files, 
+                                   folderpath, 
+                                   report_name, 
+                                   column)
 
         return be, step
 
-    def boltzmann_weighted(be, T, steps=[]):
+    def boltzmann_weighted(be, 
+                           T, 
+                           steps=[]):
         """
         Function
         ----------
@@ -219,7 +293,67 @@ def statistics(input_folder,
 
         return ene_bz
 
-    def step_weighted(be, step, pele_steps):
+    def pelesteps_retriever():
+        """
+        Function
+        ----------
+        Reads the pelesteps from adaptive.conf or pele.conf.
+
+        Returns
+        ----------
+        - pele_steps : int
+            Number of pele steps of the simulation(s).
+        """
+
+        path = str(pathlib.Path().absolute())
+        adaptive_path = os.path.join(path, 'adaptive.conf')
+
+        if os.path.isfile(adaptive_path) == True:
+
+            #
+            print('     -   Retrieving information from adaptive.conf.')
+            #
+
+            with open(adaptive_path) as filein:
+
+                for line in filein:
+
+                    if "peleSteps" in line:
+
+                        peleSteps_string = line.split()[2]
+                        pele_steps = int(peleSteps_string.split(',')[0])
+
+        else: 
+
+            peleconf_path = os.path.join(path, 'pele.conf')
+
+            #
+            print('     -   Retrieving information from pele.conf.')
+            #
+
+            if os.path.isfile(peleconf_path) == True:
+
+                with open(peleconf_path) as filein:
+
+                    for line in filein:
+
+                        if "numberOfPeleSteps" in line:
+
+                            pele_steps = int(line.split()[-1])
+
+            else: 
+
+                #
+                print('     -   No .conf was found.')
+                print('     -   The step weighted scoring function will not \n'
+                      '         be taken into account.')
+                #
+
+        return pele_steps
+
+    def step_weighted(be, 
+                      step, 
+                      pele_steps):
         """
         Function
         ----------
@@ -271,16 +405,23 @@ def statistics(input_folder,
 
         return ene_step, num_steps
 
+    #
+    print(' ')
+    print('**************************************************************')
+    print('*                           DiSC                             *')
+    print('**************************************************************')
+    print(' ')
+    #
+    
     path = str(pathlib.Path().absolute())
     folderpath = os.path.join(path, input_folder)
 
-    if os.path.isdir(folderpath) == False:
-        raise Exception(
-            'FolderPathError: There is no folder with this name. Please check the path and the folder name.')
-
     files = os.listdir(folderpath)
 
-    be, step = reader(files, folderpath, column)
+    be, step = reader(files, 
+                      folderpath, 
+                      report_name,
+                      column)
 
     minimum_energy = min(be)
     be = np.array(be)
@@ -289,20 +430,40 @@ def statistics(input_folder,
     ene_bz = boltzmann_weighted(be, T)
     ene_bz4 = boltzmann_weighted(be4, T)
     ene_step_bz = boltzmann_weighted(be, T, step)
-    ene_step, _ = step_weighted(be, step, pele_steps)
+
+    if pele_steps == None:
+
+        #
+        print(' -   No information about pele_steps was given.')
+        #
+        
+        pele_steps = pelesteps_retriever()
+
+        if pele_steps == None: 
+            
+            ene_step = 0.  
+
+        else: 
+
+            ene_step, _ = step_weighted(be, step, pele_steps)   
+
+        #
+        print(' -   Pele steps:', pele_steps)
+        #   
+
+    else: 
+  
+        ene_step, _ = step_weighted(be, step, pele_steps)
 
     #
     print(' ')
-    print('**************************************************************')
-    print('*                        ENERGIES                            *')
-    print('**************************************************************')
-    print(' ')
-    print('- Minimum Binding Energy:', minimum_energy)
-    print('- Average Binding Energy:', np.average(be))
-    print('- Boltzmann weighted Energy:', ene_bz)
-    print('- Step weighted Energy:', ene_step)
-    print('- Step-Boltzmann weighted Energy:', ene_step_bz)
-    print('- Boltzmann weighted corrected Energy:', ene_bz4)
+    print(' RESULTS:')
+    print(' -   Minimum Binding Energy:', minimum_energy)
+    print(' -   Average Binding Energy:', np.average(be))
+    print(' -   Boltzmann weighted Energy:', ene_bz)
+    print(' -   Step weighted Energy:', ene_step)
+    print(' -   Step-Boltzmann weighted Energy:', ene_step_bz)
+    print(' -   Boltzmann weighted corrected Energy:', ene_bz4)
     print(' ')
     #
 
