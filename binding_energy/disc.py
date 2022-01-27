16fa1750025099dab4455900aa443a69334b0c28
Ignasi Puch Giner <ignasi.puch.giner@gmail.com>
2022-01-27 13:13:41 +0100

    (1) Implementation of actions: all and evolution. (2) Implementation of function simulation_evolution and plotter inside statistics function.

--- binding_energy/disc.py
+++ binding_energy/disc.py
@@ -12,6 +12,8 @@ import pathlib
 import argparse
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+
 
 # ----------------------------------------------------------------------- #
 # Constants:
@@ -42,7 +44,7 @@ def parse_args(args):
                         default='output', help="Name of the output directory where the files to\
         analyze are located.")
     parser.add_argument("-rn", "--report_name", type=str, dest="report_name",
-                        default='report_', help="Name of the report files.")
+                        default='report', help="Name of the report files.")
     parser.add_argument("-T", "--temperature", type=float, dest="temperature",
                         default=298., help="Temperature of the experiment.")
     parser.add_argument("-pS", "--pele_Steps", type=int, dest="pele_steps",
@@ -50,6 +52,8 @@ def parse_args(args):
     parser.add_argument("-c", "--column", type=int, dest="column",
                         default=None, help="Column of the report where the interesting\
                              metric is located.")
+    parser.add_argument("-a", "--action", type=str, dest="action",
+                    default='all', help="Function the user wants the script to do: all or evolution.")    
 
     parsed_args = parser.parse_args(args)
 
@@ -60,7 +64,8 @@ def statistics(input_folder,
                report_name,
                T,
                pele_steps,
-               column):
+               column,
+               action):
     """
     Function
     ----------
@@ -212,7 +217,7 @@ def statistics(input_folder,
             if column == None:
 
                 column_file = os.path.join(
-                    folderpath, numeric_files[0], report_name + '1')
+                    folderpath, numeric_files[0], report_name + '_1')
                 column = column_retriever(column_file,
                                           column)
 
@@ -407,6 +412,255 @@ def statistics(input_folder,
 
         return ene_step, num_steps
 
+    def simulation_evolution(files,
+                             report_name,
+                             pele_steps,
+                             column,
+                             folderpath):
+        """
+        Function
+        ----------
+        Calculates the evolution of the boltzmann weighted value  and the minimum value 
+        of the metric chosen with column and stores data in two dictionaries.
+
+        Parameters
+        ----------
+        - files : list
+            The path to the directory where the output of the PELE simulation is located.
+        - report_name : str
+            Name of the reports to obtain the data from
+        - pele_steps : int
+            Number of pele steps in each epoch.
+        - column : int 
+            Column where the interesting data is located. 
+        - folderpath : str
+            Path where the different epochs of the simulation are located.
+
+        Returns
+        ----------
+        - bz_dict : dict
+            Dictionary with the acummulated boltzmann weighted values of the chosen metric 
+            per epoch.
+        - min_dict : dict
+            Dictionary with the accumulated minium values of the chosen metric per epoch.
+        - step_dict :dict
+            Dictionary with the number of maximum accepted steps per epoch.
+        """
+
+        def step_data_reader(files,
+                             report_name,
+                             column,
+                             folderpath,
+                             step,
+                             step_data):
+            """
+            Function
+            ----------
+            Opens all reports and extracts information from the column inputed by the user
+            at a certain step.
+
+            Parameters
+            ----------
+            - files : list
+                The path to the directory where the output of the PELE simulation is located.
+            - report_name : str
+                Name of the reports to obtain the data from
+            - column : int 
+                Column where the interesting data is located. 
+            - folderpath : str
+                Path where the different epochs of the simulation are located.
+            - step : int
+                Accepted step we are retrieving data from.
+            - step_data : list
+                List with all the accumulated data of previous steps.
+
+            Returns
+            ----------
+            - step : int
+                Accepted step next iteration is going to retrieve data from.
+            - step_data : list
+                List with all the accumulated data of previous steps.
+            """
+
+            for file in files:
+
+                cont = 0
+
+                if file.startswith(report_name):
+
+                    with open(os.path.join(folderpath,file)) as filein:
+
+                        for line in filein:
+
+                            if cont != 0:
+
+                                line = line.split()
+
+                                if step == int(line[2]):
+
+                                    step_data.append(float(line[column - 1])) 
+
+                            cont +=1
+                        
+                        else:
+
+                            continue
+
+            step += 1                  
+
+            return step_data, step
+
+        numeric_files = [s for s in files if s.isnumeric()]
+
+        step_dict = {}
+        bz_dict = {}
+        min_dict = {}
+
+        if len(numeric_files) != 0:
+
+            #
+            print(' -   Calculating...')
+            #
+            
+            for document in numeric_files:
+
+                step = 0
+
+                step_data = []
+                step_list = []
+                minimum_energy_list = []
+                bz_energy_list = []
+
+                new_directory = os.path.join(folderpath, document)
+
+                if os.path.isdir(new_directory) and document.isnumeric():
+
+                    files = os.listdir(new_directory)
+
+                    while step < pele_steps:
+
+                        if step > 1 and bz_energy_list[-1] == bz_energy_list[-2]: 
+
+                            break
+
+                        step_list.append(int(step))
+                        step_data, step = step_data_reader(files,
+                                                           report_name,
+                                                           column,
+                                                           new_directory,
+                                                           step,
+                                                           step_data)
+
+                        ene_bz = boltzmann_weighted(np.array(step_data), T)
+                        minimum_energy = min(np.array(step_data))   
+
+                        bz_energy_list.append(ene_bz)
+                        minimum_energy_list.append(minimum_energy)
+
+                step_dict[int(document)] = step_list
+                bz_dict[int(document)] = bz_energy_list
+                min_dict[int(document)] = minimum_energy_list
+
+        else:
+
+            step = 0
+
+            minimum_energy_list = []
+            bz_energy_list = []
+            step_data = []
+            step_list = []
+
+            #
+            print(' -   Calculating...')
+            #
+
+            while step < pele_steps:
+
+                if step > 1 and bz_energy_list[-1] == bz_energy_list[-2]: 
+
+                    print(' -   Maximum number of acepted steps in any report:', step - 1)
+
+                    break
+
+                step_list.append(int(step))
+                step_data, step = step_data_reader(files,
+                                                   report_name,
+                                                   column,
+                                                   folderpath,
+                                                   step,
+                                                   step_data)
+
+                ene_bz = boltzmann_weighted(np.array(step_data), T)
+                minimum_energy = min(np.array(step_data))
+
+                bz_energy_list.append(ene_bz)
+                minimum_energy_list.append(minimum_energy)
+
+            step_dict[0] = step_list
+            bz_dict[0] = bz_energy_list
+            min_dict[0] = minimum_energy_list
+                
+        return bz_dict, min_dict, step_dict
+
+    def plotter(step_dict,
+                bz_dict,
+                min_dict,
+                path,
+                input_folder):
+
+        """
+        Function
+        ----------
+        Orders dictionaries by epochs and plots the data calculated to save the results
+        in a newu directory /evolution.
+        
+        Parameters
+        ----------
+        - bz_dict : dict
+            Dictionary with the acummulated boltzmann weighted values of the chosen metric 
+            per epoch.
+        - min_dict : dict
+            Dictionary with the accumulated minium values of the chosen metric per epoch.
+        - step_dict :dict
+            Dictionary with the number of maximum accepted steps per epoch.
+        - folderpath : str
+            Path where the different epochs of the simulation are located.
+        - path : str
+            Path to the working directory.
+        - input_folder : str
+            Name of the output directory from which we have obtained the data 
+            of the dictionaries.
+
+        Returns
+        ----------
+        - step : int
+            Accepted step next iteration is going to retrieve data from.
+        - step_data : list
+            List with all the accumulated data of previous steps.
+        """
+
+        path_plots = os.path.join(path,'evolution')
+
+        if os.path.exists(path_plots) == False:
+            os.mkdir(path_plots)
+
+        step_dict = dict(sorted(step_dict.items()))
+        bz_dict = dict(sorted(bz_dict.items()))
+        min_dict = dict(sorted(min_dict.items()))
+
+        fig1, ax1 = plt.subplots()
+        fig2, ax2 = plt.subplots()
+
+        for key in step_dict:
+
+            ax1.plot(step_dict[key],bz_dict[key], label = key)
+            ax1.legend(loc = 'best', fontsize=5)
+            ax2.plot(step_dict[key],min_dict[key], label = key)
+            ax2.legend(loc = 'best', fontsize=5)
+            
+        fig1.savefig(os.path.join(path_plots, input_folder + '_bz_evolution.png'))
+        fig2.savefig(os.path.join(path_plots, input_folder + '_min_evolution.png'))
+
     #
     print(' ')
     print('**************************************************************')
@@ -420,62 +674,95 @@ def statistics(input_folder,
 
     files = os.listdir(folderpath)
 
-    be, step = reader(files,
-                      folderpath,
-                      report_name,
-                      column)
+    if action == 'all':
 
-    minimum_energy = min(be)
-    be = np.array(be)
-    be4 = be/4
+        be, step = reader(files,
+                          folderpath,
+                          report_name,
+                          column)
 
-    ene_bz = boltzmann_weighted(be, T)
-    ene_bz4 = boltzmann_weighted(be4, T)
-    ene_step_bz = boltzmann_weighted(be, T, step)
+        minimum_energy = min(be)
+        be = np.array(be)
+        be4 = be/4
 
-    if pele_steps == None:
-
-        #
-        print(' -   No information about pele_steps was given.')
-        #
-
-        pele_steps = pelesteps_retriever()
+        ene_bz = boltzmann_weighted(be, T)
+        ene_bz4 = boltzmann_weighted(be4, T)
+        ene_step_bz = boltzmann_weighted(be, T, step)
 
         if pele_steps == None:
 
-            ene_step = 0.
+            #
+            print(' -   No information about pele_steps was given.')
+            #
+
+            pele_steps = pelesteps_retriever()
+
+            if pele_steps == None:
+
+                ene_step = 0.
+
+            else:
+
+                ene_step, _ = step_weighted(be, step, pele_steps)
+
+            #
+            print(' -   Pele steps:', pele_steps)
+            #
 
         else:
 
             ene_step, _ = step_weighted(be, step, pele_steps)
 
         #
-        print(' -   Pele steps:', pele_steps)
+        print(' ')
+        print(' RESULTS:')
+        print(' -   Minimum Binding Energy:', minimum_energy)
+        print(' -   Average Binding Energy:', np.average(be))
+        print(' -   Boltzmann weighted Energy:', ene_bz)
+        print(' -   Step weighted Energy:', ene_step)
+        print(' -   Step-Boltzmann weighted Energy:', ene_step_bz)
+        print(' -   Boltzmann weighted corrected Energy:', ene_bz4)
+        print(' ')
         #
 
-    else:
+        with open('energy.csv', 'w') as fileout:
+            fileout.writelines(
+                'Minimum,Average,Boltzmann weighted,Step weighted,Step-Boltzmann weighted,Boltzmann weighted corrected\n'
+                '' + str(minimum_energy) + ',' + str(np.average(be)) + ',' + str(ene_bz) +
+                ',' + str(ene_step) + ',' + str(ene_step_bz) +
+                ',' + str(ene_bz4) + '\n'
+            )
+    
+    elif action == 'evolution':
 
-        ene_step, _ = step_weighted(be, step, pele_steps)
+        if pele_steps == None:
 
-    #
-    print(' ')
-    print(' RESULTS:')
-    print(' -   Minimum Binding Energy:', minimum_energy)
-    print(' -   Average Binding Energy:', np.average(be))
-    print(' -   Boltzmann weighted Energy:', ene_bz)
-    print(' -   Step weighted Energy:', ene_step)
-    print(' -   Step-Boltzmann weighted Energy:', ene_step_bz)
-    print(' -   Boltzmann weighted corrected Energy:', ene_bz4)
-    print(' ')
-    #
+            #
+            print(' -   No information about pele_steps was given.')
+            #
 
-    with open('energy.csv', 'w') as fileout:
-        fileout.writelines(
-            'Minimum,Average,Boltzmann weighted,Step weighted,Step-Boltzmann weighted,Boltzmann weighted corrected\n'
-            '' + str(minimum_energy) + ',' + str(np.average(be)) + ',' + str(ene_bz) +
-            ',' + str(ene_step) + ',' + str(ene_step_bz) +
-            ',' + str(ene_bz4) + '\n'
-        )
+            pele_steps = pelesteps_retriever()
+
+            #
+            print(' -   Pele steps:', pele_steps)
+            print(' ')
+            #
+
+        bz_dict, min_dict, step_dict = simulation_evolution(files,
+                                                            report_name,
+                                                            pele_steps,
+                                                            column,
+                                                            folderpath)
+
+        plotter(step_dict,bz_dict,min_dict,path,input_folder)
+
+        #
+        print(' -   Images have been stored in working directory.')
+        #
+
+    else: 
+
+        raise Exception('ActionError: The action you chose is not either all of evolution.')
 
 
 def main(args):
@@ -496,8 +783,10 @@ def main(args):
                report_name=args.report_name,
                T=args.temperature,
                pele_steps=args.pele_steps,
-               column=args.column)
+               column=args.column,
+               action=args.action)
 
+    print(' ')
     print('                    --Duration of the execution--                   ')
     print('                      %s seconds' % (time.time() - start_time))
     print(' ')
