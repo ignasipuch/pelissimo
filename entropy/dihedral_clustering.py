d2edf9b4a523067ffa0deb70f4f82f78009f5512
Ignasi Puch Giner <ignasi.puch.giner@gmail.com>
2022-01-31 15:21:43 +0100

    Rotamer library and positional information of ligand atoms

--- entropy/dihedral_clustering.py
+++ entropy/dihedral_clustering.py
@@ -13,12 +13,6 @@ import pathlib
 import argparse
 import numpy as np
 
-# ----------------------------------------------------------------------- #
-# Constants:
-T = 298.
-R = 1.985e-3
-# ----------------------------------------------------------------------- #
-
 
 def parse_args(args):
     """
@@ -45,63 +39,118 @@ def parse_args(args):
         is located.")
     parser.add_argument("-r", "--residue_name", type=str, dest="residue_name",
                         default='LIG', help="Ligand's residue name.")
-    parser.add_argument("-c", "--conf_name", type=str, dest="conf_name",
-                        default='pele', help="Name of the conf file used for the simulation.")
 
     parsed_args = parser.parse_args(args)
 
     return parsed_args
 
-def main_function(conf_name,
+def main_function(input_folder,
                   residue_name):
 
-    def forcefield_retriever(conf_name):
+    def path_definer(input_folder):
 
         path = str(pathlib.Path().absolute())
+        path_template = os.path.join(path,'DataLocal','LigandRotamerLibs')
+        path_output = os.path.join(path,input_folder)
 
-        with open(os.path.join(path,conf_name + '.conf')) as filein:
-
-            for line in filein:
-
-                if "ForceField" in line:
-
-                    line = line.split(':')
-                    line = line[1].split('     ')
-                    line = line[0].split('"')
-                    forcefield = line[1]
-
-        return forcefield
-
-
-    def path_definer(forcefield):
-
-        path = str(pathlib.Path().absolute())
-
-        if forcefield == 'OPLS2005':
-
-            path_template = os.path.join(path,'DataLocal','Templates',forcefield,'HeteroAtoms')
-
-        elif forcefield == 'OpenFF-OPLS2005':
-
-            path_template = os.path.join(path,'DataLocal','Templates','OpenFF','Parsley')
-
-        else: 
-
-            raise Exception('ForceFieldError: The force field read in the .conf is not valid: OPLS2005 or OpenFF-OPLS2005.')
-
-        return path_template
-        
-
+        return path_template, path_output
+      
     def template_info_retriever(path_template,
                                 residue_name):
 
-        with open(os.path.join(path_template,residue_name.lower() + 'z'))
+        dihedral_angles_dict = {}
 
-           
+        with open(os.path.join(path_template,residue_name + '.rot.assign')) as filein:
+
+            for line in filein:
+
+                line = line.split()
+
+                if line[0].startswith('sidelib'):
+
+                    angle = int(line[1].split('E')[-1])
+                    dihedral_angles_dict[tuple(line[2:-1])] = angle
+
+        return dihedral_angles_dict
+
+    def trajectory_positions(path_output):
+        
+        def trajectory_positions_retriever(path,
+                                           file_list):
+
+            trajectory_dict = {}
+            epoch_dict = {}
+
+            file_list = [k for k in file_list if 'trajectory' in k]
+
+            for file in file_list:
+
+                if 'trajectory' in file:
+
+                    trajectory_number = file.split('_')[-1].split('.pdb')[0]
+                    atom_position = {}
+
+                    with open(os.path.join(path,file)) as filein:
+
+                        for line in filein:
+
+                            if 'MODEL' in line:
+
+                                line = line.split()
+                                model = line[-1]
+
+                            elif residue_name in line:
+
+                                line = line.split()
+                                atom_position[line[2]] = tuple([line[5],line[6],line[7]])
+
+                            elif 'ENDMDL' in line:
+
+                                trajectory_dict[model] = atom_position
+                    
+                    epoch_dict[trajectory_number] =  trajectory_dict
+
+                
+            return epoch_dict
+        
+        files = os.listdir(path_output)
+        numeric_files = [s for s in files if s.isnumeric()]
+
+        if len(numeric_files) != 0:
+
+            simulation_dict = {}
+
+            for document in numeric_files:
+
+                new_directory = os.path.join(path_output, document)
+
+                if os.path.isdir(new_directory) and document.isnumeric():
+
+                    files = os.listdir(new_directory)       
+
+                    simulation_dict[document] = trajectory_positions_retriever(new_directory,
+                                                                          files)
+
+        else:
+
+            simulation_dict = trajectory_positions_retriever(path_output,
+                                                        files)
+        
+        return simulation_dict
+
+    def dihedral_angle(dihedral_angles_dict,
+                       simulation_dict):
+
+        
 
 
 
 
+        
+    path_template, path_output = path_definer(input_folder)
+    dihedral_angles_dict = template_info_retriever(path_template,
+                                                   residue_name)
+    simulation_dict = trajectory_positions(path_output)
 
 def main(args):
     """
@@ -115,8 +164,8 @@ def main(args):
         It contains the command-line arguments that are supplied by the user
     """
 
-    main_function(conf_name=args.conf_name)
-
+    main_function(input_folder=args.input_folder,
+                  residue_name=args.residue_name)
 
 if __name__ == '__main__':
 
