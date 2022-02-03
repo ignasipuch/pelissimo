8011c81fbd0163d5a984f8752934208d11a7a5d3
Ignasi Puch Giner <ignasi.puch.giner@gmail.com>
2022-02-03 15:55:53 +0100

    Functions informatin and autopep8

--- entropy/dihedral_clustering.py
+++ entropy/dihedral_clustering.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This cluster trajectories with dihedrals
+This script clusters trajectories depending on dihedral information
 """
 
 __author__ = "Ignasi Puch-Giner"
@@ -15,12 +15,12 @@ import numpy as np
 
 from rdkit import Chem
 
+
 def parse_args(args):
     """
     Function
     ----------
     It parses the command-line arguments.
-    Parameters
 
     Parameters
     ----------
@@ -45,23 +45,75 @@ def parse_args(args):
 
     return parsed_args
 
+
 def main_function(input_folder,
                   residue_name):
+    """
+    Function
+    ----------
+    Calculates the important dihedrals of all the conformations in a PELE 
+    simulation. 
+
+    Parameters
+    ----------
+    - input_folder : str
+        Name of the folder where the output of the simulation is located.
+    - residue_name : str
+        Residue name of the ligand.
+
+    Returns
+    ----------
+
+    """
 
     def path_definer(input_folder):
+        """
+        Function
+        ----------
+        Defines the important paths that are going to be used throughout the simulation.
+
+        Parameters
+        ----------
+        - input_folder : str
+            Name of the folder where the output of the simulation is located.
+
+        Returns
+        ----------
+        - path_template : str
+            Path to the rotatable bonds file.
+        - path_output : str
+            Path to the output folder of the simulation.
+        """
 
         path = str(pathlib.Path().absolute())
-        path_template = os.path.join(path,'DataLocal','LigandRotamerLibs')
-        path_output = os.path.join(path,input_folder)
+        path_template = os.path.join(path, 'DataLocal', 'LigandRotamerLibs')
+        path_output = os.path.join(path, input_folder)
 
         return path_template, path_output
-      
+
     def template_info_retriever(path_template,
                                 residue_name):
+        """
+        Function
+        ----------
+        Retrieve the information about the rotatable bonds of the ligand. 
+
+        Parameters
+        ----------
+        - path_template : str
+            Path to the rotatable bonds file.
+        - residue_name : str
+            Residue name of the ligand.
+
+        Returns
+        ----------
+        - rotatable_bonds_dict : dict
+            Dictionary with the rotatable bonds of the ligand.
+        """
 
         rotatable_bonds_dict = {}
 
-        with open(os.path.join(path_template,residue_name + '.rot.assign')) as filein:
+        with open(os.path.join(path_template, residue_name + '.rot.assign')) as filein:
 
             cont = 1
 
@@ -70,18 +122,39 @@ def main_function(input_folder,
                 line = line.split()
 
                 if line[0].startswith('sidelib'):
-                    
-                    rotatable_bonds_dict[cont] = tuple([line[2].split('_')[1],line[3].split('_')[1]])
+
+                    rotatable_bonds_dict[cont] = tuple(
+                        [line[2].split('_')[1], line[3].split('_')[1]])
                     cont += 1
 
         return rotatable_bonds_dict
 
     def atoms_to_track(residue_name,
                        rotatable_bonds_dict):
+        """
+        Function
+        ----------
+        Selects the atoms that are going to be checked to calculate dihedrals involving
+        the rotatable bonds. 
+
+        Parameters
+        ----------
+        - residue_name : str
+            Residue name of the ligand.
+        - rotatable_bonds_dict : dict
+            Dictionary with the rotatable bonds of the ligand.
+
+        Returns
+        ----------
+        - dihedral_bond_dict : dict
+            Dictionary with the atoms involved in the calculation of the dihedrals.
+        - atom_list : list
+            List with all the atoms without repetition that have to be tracked.
+        """
 
         path = str(pathlib.Path().absolute())
-        path_input = os.path.join(path,'input')
-        path_ligand = os.path.join(path_input,'ligand.pdb')
+        path_input = os.path.join(path, 'input')
+        path_ligand = os.path.join(path_input, 'ligand.pdb')
 
         atoms = []
 
@@ -91,7 +164,7 @@ def main_function(input_folder,
 
                 if residue_name in line:
 
-                    atom = line.split()[2]                    
+                    atom = line.split()[2]
                     atoms.append(atom)
 
         m = Chem.rdmolfiles.MolFromPDBFile(path_ligand)
@@ -104,21 +177,24 @@ def main_function(input_folder,
 
         for atom in range(len(heavy_atoms)):
 
-            neighbours_dict[heavy_atoms[atom]] = [atoms[x.GetIdx()] for x in m.GetAtomWithIdx(atom).GetNeighbors()]
+            neighbours_dict[heavy_atoms[atom]] = [
+                atoms[x.GetIdx()] for x in m.GetAtomWithIdx(atom).GetNeighbors()]
 
         for key in rotatable_bonds_dict:
 
             all_neighbours_atom_1 = neighbours_dict[rotatable_bonds_dict[key][0]]
             all_neighbours_atom_2 = neighbours_dict[rotatable_bonds_dict[key][1]]
 
-            neighbours_atom_1 = [x for x in all_neighbours_atom_1 if rotatable_bonds_dict[key][1] not in x]
-            neighbours_atom_2 = [x for x in all_neighbours_atom_2 if rotatable_bonds_dict[key][0] not in x]
+            neighbours_atom_1 = [
+                x for x in all_neighbours_atom_1 if rotatable_bonds_dict[key][1] not in x]
+            neighbours_atom_2 = [
+                x for x in all_neighbours_atom_2 if rotatable_bonds_dict[key][0] not in x]
 
             dihedral_bond_dict[key] = neighbours_atom_1[0],\
-                                      rotatable_bonds_dict[key][0],\
-                                      rotatable_bonds_dict[key][1],\
-                                      neighbours_atom_2[0]
-            
+                rotatable_bonds_dict[key][0],\
+                rotatable_bonds_dict[key][1],\
+                neighbours_atom_2[0]
+
             atom_list.append(neighbours_atom_1[0])
             atom_list.append(rotatable_bonds_dict[key][0])
             atom_list.append(rotatable_bonds_dict[key][1])
@@ -131,14 +207,71 @@ def main_function(input_folder,
     def trajectory_positions(path_output,
                              atom_list,
                              dihedral_bond_dict):
-        
+        """
+        Function
+        ----------
+        Calculate the dihedral bond angles for all the conformation in all epochs, 
+        trajectories and models.
+
+        Parameters
+        ----------
+        - path_output : str
+            Path to the output folder of the simulation.
+        - atom_list : list
+            List with all the atoms without repetition that have to be tracked.
+        - dihedral_bond_dict : dict
+            Dictionary with the atoms involved in the calculation of the dihedrals.
+
+        Returns
+        ----------
+
+        """
+
         def trajectory_positions_retriever(path,
                                            file_list,
                                            atom_list,
                                            dihedral_bond_dict):
+            """
+            Function
+            ----------
+            Calculate the dihedral bond angles for an epoch.
+
+            Parameters
+            ----------
+            - path : str
+                Path to an epoch of the simulation.
+            - file_list : list 
+                List of all the files in directory.
+            - atom_list : list
+                List with all the atoms without repetition that have to be tracked.
+            - dihedral_bond_dict : dict
+                Dictionary with the atoms involved in the calculation of the dihedrals.
+
+            Returns
+            ----------
+            - dihedral_angles_epoch : dict
+                Dictionary with dihedral angles information of that epoch.
+            """
 
             def dihedral_angle_calculator(dihedral_bond_dict,
                                           atoms_positions_dict):
+                """
+                Function
+                ----------
+                Calculate the dihedral angles of a conformation.
+
+                Parameters
+                ----------
+                - dihedral_bond_dict : dict
+                    Dictionary with the atoms involved in the calculation of the dihedrals.
+                - atoms_positions_dict : dict
+                    Dictionary with all the atoms of interest and their x, y, z positions.
+
+                Returns
+                ----------
+                - dihedral_angle_dict : dict
+                    Dictionary with dihedral angles information of that conformation.
+                """
 
                 dihedral_angle_dict = {}
 
@@ -149,25 +282,30 @@ def main_function(input_folder,
                     atom_name_c = dihedral_bond_dict[key][2]
                     atom_name_d = dihedral_bond_dict[key][3]
 
-                    coordinates_atom_a = np.array(atoms_positions_dict[atom_name_a])
-                    coordinates_atom_b = np.array(atoms_positions_dict[atom_name_b])
-                    coordinates_atom_c = np.array(atoms_positions_dict[atom_name_c])
-                    coordinates_atom_d = np.array(atoms_positions_dict[atom_name_d])
+                    coordinates_atom_a = np.array(
+                        atoms_positions_dict[atom_name_a])
+                    coordinates_atom_b = np.array(
+                        atoms_positions_dict[atom_name_b])
+                    coordinates_atom_c = np.array(
+                        atoms_positions_dict[atom_name_c])
+                    coordinates_atom_d = np.array(
+                        atoms_positions_dict[atom_name_d])
 
                     b1 = coordinates_atom_b - coordinates_atom_a
-                    b2 = coordinates_atom_c - coordinates_atom_b; b2_u = b2/np.linalg.norm(b2)
+                    b2 = coordinates_atom_c - coordinates_atom_b
+                    b2_u = b2/np.linalg.norm(b2)
                     b3 = coordinates_atom_d - coordinates_atom_c
 
-                    n1 = np.cross(b1,b2)/np.linalg.norm(np.cross(b1,b2))
-                    n2 = np.cross(b2,b3)/np.linalg.norm(np.cross(b2,b3))
-                    m1 = np.cross(n1,b2_u)
+                    n1 = np.cross(b1, b2)/np.linalg.norm(np.cross(b1, b2))
+                    n2 = np.cross(b2, b3)/np.linalg.norm(np.cross(b2, b3))
+                    m1 = np.cross(n1, b2_u)
 
-                    y = np.dot(m1,n2) 
-                    x = np.dot(n1,n2)
-                    
-                    dihedral_angle = -np.arctan2(y,x)*(180./np.pi)
+                    y = np.dot(m1, n2)
+                    x = np.dot(n1, n2)
+
+                    dihedral_angle = -np.arctan2(y, x)*(180./np.pi)
                     dihedral_angle_dict[key] = dihedral_angle
-                
+
                 return dihedral_angle_dict
 
             dihedral_angles_epoch = {}
@@ -180,7 +318,7 @@ def main_function(input_folder,
                     dihedral_angles_trajectory = {}
                     trajectory_number = file.split('_')[-1].split('.pdb')[0]
 
-                    filein = open(os.path.join(path,file), "r")
+                    filein = open(os.path.join(path, file), "r")
                     content = filein.read()
                     models_list = content.split('MODEL')
                     models_list = models_list[1:len(models_list)]
@@ -192,7 +330,8 @@ def main_function(input_folder,
                         atoms_positions_dict = {}
 
                         model_lines = model.splitlines(True)
-                        ligand_lines = [line for line in model_lines if 'LIG' in line] 
+                        ligand_lines = [
+                            line for line in model_lines if 'LIG' in line]
 
                         for line in ligand_lines:
 
@@ -200,18 +339,20 @@ def main_function(input_folder,
 
                                 line = line.split()
                                 atoms_positions_dict[line[2]] = [float(line[5]),
-                                                                 float(line[6]),
+                                                                 float(
+                                                                     line[6]),
                                                                  float(line[7])]
 
                         dihedral_angles_trajectory[model_cont] = dihedral_angle_calculator(dihedral_bond_dict,
-                                                                    atoms_positions_dict)
-                        
+                                                                                           atoms_positions_dict)
+
                         model_cont += 1
 
-                dihedral_angles_epoch[int(trajectory_number)] = dihedral_angles_trajectory
-        
+                dihedral_angles_epoch[int(
+                    trajectory_number)] = dihedral_angles_trajectory
+
             return dihedral_angles_epoch
-        
+
         files = os.listdir(path_output)
         numeric_files = [s for s in files if s.isnumeric()]
 
@@ -225,12 +366,12 @@ def main_function(input_folder,
 
                 if os.path.isdir(new_directory) and document.isnumeric():
 
-                    files = os.listdir(new_directory)       
+                    files = os.listdir(new_directory)
 
                     simulation_dict[int(document)] = trajectory_positions_retriever(new_directory,
-                                                                               files,
-                                                                               atom_list,
-                                                                               dihedral_bond_dict)
+                                                                                    files,
+                                                                                    atom_list,
+                                                                                    dihedral_bond_dict)
 
         else:
 
@@ -238,7 +379,7 @@ def main_function(input_folder,
                                                                 files,
                                                                 atom_list,
                                                                 dihedral_bond_dict)
-        
+
         return simulation_dict
 
     path_template, path_output = path_definer(input_folder)
@@ -252,6 +393,7 @@ def main_function(input_folder,
 
     print(simulation_dict[0][1][1])
     print(dihedral_bond_dict)
+
 
 def main(args):
     """
@@ -267,6 +409,7 @@ def main(args):
 
     main_function(input_folder=args.input_folder,
                   residue_name=args.residue_name)
+
 
 if __name__ == '__main__':
 
