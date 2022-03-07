910b1333583eb836e8e89eaf4fd66459d8aa068c
Ignasi Puch-Giner <ignasi.puchginer@bsc.es>
2022-03-07 16:48:27 +0100

    Deletion of all but code

--- strain/LIG_linen_cry/script.py
+++
@@ -1,4 +0,0 @@
-from pele_platform.analysis import Analysis
-
-analysis = Analysis(resname="LIG", chain="L", simulation_output="output", report="report", cpus=48)
-analysis.generate(path="analysis", clustering_type="meanshift")
